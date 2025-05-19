e9b02f95c16d5b1257dea4724e2797b93abe93d3
OperatorAlexCode <alexander.jr.karlsson@gmail.com>
2025-05-19 13:20:46 +0200

    Mazes are now able to be displayed visually

--- app/src/main/java/com/example/assignmentc/MainActivity.kt
+++ app/src/main/java/com/example/assignmentc/MainActivity.kt
@@ -1,16 +1,43 @@
 package com.example.assignmentc
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asComposePath
+import androidx.compose.ui.graphics.drawscope.draw
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.graphics.shapes.RoundedPolygon
+import androidx.graphics.shapes.toPath
 import com.example.assignmentc.ui.theme.AssignmentCTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,12 +45,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            var maze by remember { mutableStateOf(Maze()) }
+
             AssignmentCTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(modifier = Modifier.fillMaxSize().padding(innerPadding)) {
+                        MazeDisplay(Modifier.align(Alignment.Center),maze)
+                    }
                 }
             }
         }
@@ -38,10 +66,90 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun MazeDisplay(modifier: Modifier,toDisplay: Maze) {
+    val dimension = 300.dp
+    val margin = 4.dp
+
+    /*Box(modifier = modifier.size(dimension)) {
+        LazyColumn(modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(margin),
+            horizontalAlignment = Alignment.CenterHorizontally)
+        {
+            items(toDisplay.Tiles) { row ->
+                LazyRow(modifier = Modifier.fillMaxSize(),
+                    horizontalArrangement = Arrangement.spacedBy(margin),
+                    verticalAlignment = Alignment.CenterVertically)
+                {
+                    items(row)
+                    { item ->
+                        var tileColor = Color(0xFFBDBDBD)
+
+                        if (item.IsWall)
+                            tileColor = Color(0xFF3D3D3D)
+
+                        /*Box(modifier = Modifier.size(20.dp).drawWithCache {
+                            val tile = RoundedPolygon(
+                                numVertices = 4,
+                                radius = size.minDimension/2,
+                                centerX = size.width/2,
+                                centerY = size.height/2
+                            )
+                            //Log.d("Shape", "Drawing shape $tile")
+                            val path = tile.toPath().asComposePath()
+                            onDrawBehind {
+                                drawPath(path,color = tileColor)
+                            }
+                        }.fillMaxSize())*/
+
+                        Box(modifier = Modifier.background(tileColor).fillMaxSize().defaultMinSize(10.dp,10.dp))
+                    }
+                }
+            }
+        }
+    }*/
+
+    Box(modifier = modifier.size(dimension)) {
+        Column(modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(margin),
+            horizontalAlignment = Alignment.CenterHorizontally)
+        {
+            for (y in 0..toDisplay.Width-1)
+            {
+                Row(modifier = Modifier.fillMaxSize().weight(1f),
+                    horizontalArrangement = Arrangement.spacedBy(margin),
+                    verticalAlignment = Alignment.CenterVertically) {
+                    for (x in 0..toDisplay.Height-1){
+                        var tileColor = Color(0xFFBDBDBD)
+
+                        if (toDisplay.Tiles[x][y].IsWall)
+                            tileColor = Color(0xFF3D3D3D)
+
+                        /*Box(modifier = Modifier.size(20.dp).drawWithCache {
+                            val tile = RoundedPolygon(
+                                numVertices = 4,
+                                radius = size.minDimension/2,
+                                centerX = size.width/2,
+                                centerY = size.height/2
+                            )
+                            //Log.d("Shape", "Drawing shape $tile")
+                            val path = tile.toPath().asComposePath()
+                            onDrawBehind {
+                                drawPath(path,color = tileColor)
+                            }
+                        }.fillMaxSize())*/
+
+                        Box(modifier = Modifier.weight(1f).background(tileColor).fillMaxSize().defaultMinSize(10.dp,10.dp))
+                    }
+                }
+            }
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AssignmentCTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/assignmentc/Maze.kt
+++ app/src/main/java/com/example/assignmentc/Maze.kt
@@ -2,26 +2,60 @@ package com.example.assignmentc
 
 class Maze {
 
-    val Height: Int = 11
-    val Width: Int = 11
+    var Height: Int = 11
+    var Width: Int = 11
+    // 2D array of the tiles where:
+    // 0,0 is the upper left corner,
+    // Width-1, 0 is the upper right corner,
+    // 0, Height-1 is the lower left corner and
+    // Width-1, Height-1 is the lower right corner
     var Tiles: Array<Array<Tile>> = emptyArray()
 
-    init {
+    constructor() {
         Construct()
+        ConnectTiles()
     }
 
-    // Constructs the maze using a given instruction
+    constructor(height: Int, width:Int) {
+        Height = height
+        Width = width
+        Construct()
+        ConnectTiles()
+    }
+
+    // Default constructor that is used to create the maze using a given instruction
     fun Construct() {
         Tiles = Array(Height) { emptyArray() }
 
-        for (y in 0..Height-1){
-            var row = emptyArray<Tile>()
+        for (x in 0..Width-1){
+            var column = emptyArray<Tile>()
 
-            for (x in 0..Width-1){
-                row += Tile(x,y,false)
+            for (y in 0..Height-1){
+                column += Tile(x,y,if ((x == 0 || x == Width-1) || (y == 0 || y == Height-1) || (x % 2 == 0 && y % 2 == 0)) true else false)
             }
 
-            Tiles[y] = row
+            Tiles[x] = column
         }
+    }
+
+    fun ConnectTiles() {
+        for (x in 0..Width-1)
+            for (y in 0.. Height-1) {
+                if (y - 1 >= 0)
+                    if (!Tiles[x][y-1].IsWall)
+                        Tiles[x][y].NorthTile = Tiles[x][y-1]
+
+                if (y + 1 < Height)
+                    if (!Tiles[x][y+1].IsWall)
+                        Tiles[x][y].SouthTile = Tiles[x][y+1]
+
+                if (x - 1 >= 0)
+                    if (!Tiles[x-1][y].IsWall)
+                        Tiles[x][y].WestTile = Tiles[x-1][y]
+
+                if (x + 1 < Width)
+                    if (!Tiles[x+1][y].IsWall)
+                        Tiles[x][y].EastTile = Tiles[x+1][y]
+            }
     }
 }
