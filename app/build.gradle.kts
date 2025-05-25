3d4613a25403529f8757b8727d2c079d15ccb67f
TianK003 <tian.kljucanin@gmail.com>
2025-05-25 13:28:48 +0200

    Updated dependencies to support room database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -56,5 +57,13 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation("androidx.graphics:graphics-shapes:1.0.1")
+    implementation(libs.androidx.graphics.shapes)
+    // Room
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+    // ViewModel
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    // Coroutines
+    implementation(libs.kotlinx.coroutines.android)
 }
